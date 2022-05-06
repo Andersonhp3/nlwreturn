ad70571a23c37b1c4005f9b0a4d8577627ea4a29
Andersonhp3 <arhp1591@gmail.com>
2022-05-06 00:06:24 -0300

    Aula 03 finalizada

---
+++ main.js
@@ -0,0 +1,34 @@
+function onScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+  console.log('aqui')
+}
+
+
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`#home,
+  #home img,
+  #home .stats,
+  #services,
+  #services header,
+  #services cards,
+  #about,
+  #about header,
+  #about content,
+  `);
